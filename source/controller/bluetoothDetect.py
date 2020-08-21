6f0f873ce35c33286032d86214956233ef17182a
Tien <s3757934@rmit.edu.vn>
2020-08-21 12:08:13 +0700

    set up detect engineer by bluetooth id (mac address)

--- source/controller/bluetoothDetect.py
+++ source/controller/bluetoothDetect.py
@@ -3,17 +3,17 @@
 import bluetooth
 from time import sleep
 
-# Search for device containing keyword
-def start_searching(server_mac_address = "", server_name = "CuongvTien"):
-    while True:
-        # Wait until bluetooth is on and stable
+# Search for engineer device with matched mac address
+def start_searching(device_mac_address = ""):
+    # Wait until bluetooth is on and stable (loop throught the unstable period)
+    while True:         
         try:
             sleep(5)
             # Get all nearby devices mac address
             nearby_devices_mac_address = bluetooth.discover_devices()
             # Loop through all nearby devices
             for mac_address in nearby_devices_mac_address:
-                if mac_address == server_mac_address:
+                if mac_address == device_mac_address:
                     engineer_found = True
             if engineer_found:
                 pass
