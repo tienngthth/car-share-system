389d45acb6e4b6d41e5848e5313f572d76732060
Tam Nguyen Thanh <S3747274@rmit.edu.vn>
2020-08-22 20:14:47 +0700

    add a method to connect to the GCP

--- connect.py
+++ connect.py
@@ -1,6 +1,10 @@
 import pymysql
 
-#cloud_sql_proxy -instances=clean-wonder-286803:asia-southeast2:s3747274=tcp:3306
+#To connect to the GCP database:
+#1) Download the Cloud SQL Proxy
+#2) Download the JSON file included in this project
+#3) In the command prompt, navigate to the location of the proxy and run this command:
+#cloud_sql_proxy -instances=clean-wonder-286803:asia-southeast2:s3747274=tcp:3306 -credential_file=<PATH TO THE JSON FILE>
 
 def connectToDatabase():
     connection = pymysql.connect(host='127.0.0.1',
