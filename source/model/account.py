c0376cbbee1c67bc3546c797b2bc02cb442efd5f
Tien <s3757934@rmit.edu.vn>
2020-08-26 14:57:34 +0700

    check for existed username

--- source/model/account.py
+++ source/model/account.py
@@ -4,30 +4,36 @@ from abc import ABC, ABCMeta, abstractmethod
 from database import Database
 
 class Account(ABC):
-    def __init__(self, username, password, email, first_name, last_name):
+    def __init__(self, username, password, email, first_name, last_name, user_type):
         super().__init__()
         self.__username = username
         self.__password = Account.hash_salt_password(password)
         self.__first_name = first_name
         self.__last_name = last_name
         self.__email = email
+        self.__user_type = user_type
 
     @staticmethod
     def hash_salt_password(raw_input):
         return hash.sha256_crypt.hash(raw_input)
 
     @staticmethod
-    def verify_password(input_password, username, use_type):
-        if user_type == "staff" and Database.select_record("Staff", )
+    def verify_password(input_password, username, user_type):
+        #user_type == Staff/Customer
         #retrieve password from database by username
-        encryptedPassword = ""
+        encryptedPassword = Database.select_record("Password", user_type, "WHERE Username=(%s)," + username)
         return hash.sha256_crypt.verify(input_password, encryptedPassword)
 
     @staticmethod
-    def validate_username_input(username):
+    def validate_username_input(username, user_type):
+        #user_type == Staff/Customer
         # Valid username is unique and contains 6-15 alphanumerical characters 
-        existed_username = True     #Need to check from the database
-        if existed_username and re.search("^[A-Za-z0-9]{6,15}$", username): 
+        existed_username = Database.execute_equation(
+            "COUNT(*)", 
+            user_type, 
+            "WHERE username=" + username
+        ) > 0
+        if not existed_username and re.search("^[A-Za-z0-9]{6,15}$", username): 
             return True
         return False
 
@@ -53,3 +59,19 @@ class Account(ABC):
         if re.search("^([A-Za-z0-9]+([.]|[_])?[A-Za-z0-9]+)+[@][A-Za-z]+[.][A-Za-z]{2,3}$", email):
             return True
         return False
+
+    #Login to the database
+    def __log_data_to_db(self):
+        try:
+            parameters = (
+                self.__username, self.__password, 
+                self.__first_name, self.__last_name, 
+                self.__email, self.__user_type
+            )
+            Database.insert_record(
+                "Staff (Username, Password, FirstName, LastName, Email, UserType)",
+                "(%s, %s, %s, %s, %s, %s)",
+                parameters
+            )
+        except:
+            pass
