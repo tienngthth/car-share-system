1df98f36116e6f9f6837354f414d49c308274a76
Tien <s3757934@rmit.edu.vn>
2020-08-30 16:35:39 +0700

    commit

--- source/master_socket.py
+++ source/master_socket.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from model.client import Client
 from model.server import Server
 from model.code import Code
 from model.account import Account
@@ -15,17 +14,16 @@ def run_server():
     #Receive message from client -> validate/update car status
     try:
         message = Code.parse_json(server.receive_message())
-        if message["type"] == credential:
+        if message["message_type"] == "credential":
             validate_crendential()
-        elif message["type"] == car_status:
+        elif message["message_type"] == "car_status":
             update_car_status()
     except:
         pass
 
-
 # Validate credential
 def validate_crendential():
-    if Account.verify_password(message["username"], message["password"]):
+    if Account.verify_password(message["username"], message["password"], message["user_type"]):
         server.send_message("valid")
     else:
         server.send_message("invalid")
@@ -35,11 +33,6 @@ def update_car_status():
     #Up car availability - user first login/return
     pass
 
-# Function to call when Pi is a client
-def run_client():
-    client = Client()
-    # do something
-    client.close_socket()
 
 if __name__ == "__main__":
     run_server()
