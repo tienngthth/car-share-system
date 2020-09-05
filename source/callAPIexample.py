505ba85481684ea8088c08052f04a01bdfc95275
tienngthth <s3757934@rmit.edu.vn>
2020-09-05 15:40:04 +0700

    cm

--- source/callAPIexample.py
+++ source/callAPIexample.py
@@ -7,7 +7,7 @@ def test_1():
 
 # Function to call GET API to get the latest context from the database
 def test_2():
-    resp = requests.get('http://127.0.0.1:8080/cars/create?mac_address=&brand=ABC&type=&location=&status=to%20be%20repaired&color=&seat=5&cost=50')
+    resp = requests.post('http://127.0.0.1:8080/cars/create?mac_address=&brand=ABC&type=&location=&status=to%20be%20repaired&color=&seat=5&cost=50')
     print(resp.text)
 
 
