3c914d26b9c2bf417aa9277b1f8c4d3df9957dad
cuong24 <s3748840@rmit.edu.vn>
2020-09-05 19:06:58 +0700

    có bluetooth

---
+++ source/bluetoothDetect.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import bluetooth
+from time import sleep
+
+# Search for engineer device with matched mac address
+def start_searching(device_mac_address = ""):
+    # Wait until bluetooth is on and stable (loop throught the unstable period)
+    while True:         
+        try:
+            sleep(5)
+            # Get all nearby devices mac address
+            nearby_devices_mac_address = bluetooth.discover_devices()
+            # Loop through all nearby devices
+            for mac_address in nearby_devices_mac_address:
+                if mac_address == device_mac_address:
+                    engineer_found = True
+            if engineer_found:
+                pass
+                #Unlock car
+            else:
+                #Lock car
+                pass
+        except:
+            start_searching()
+
+if __name__ == "__main__":
+    start_searching()
