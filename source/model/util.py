41bd7b1c6a7769e33d7f82116fab5f23c6a38141
Tien Nguyen <tien.nguyen@skedulo.com>
2020-09-09 08:51:04 +0700

    done pagination

--- source/model/util.py
+++ source/model/util.py
@@ -1,18 +1,18 @@
 class Util:
 
     @staticmethod
-    def paginatedDisplay(recordList, page):
-        indices = Util.getIndices(len(recordList), page)
+    def paginatedDisplay(recordList, page, maximum_record_per_page = 5):
+        indices = Util.getIndices(len(recordList), page, maximum_record_per_page)
         return recordList[indices[0]: indices[1]]
     
     @staticmethod
-    def getIndices(listSize, page):
+    def getIndices(listSize, page, maximum_record_per_page):
         if (page == 0):
             return (0, listSize)
-        firstIndex = (page - 1) * 5
+        firstIndex = (page - 1) * maximum_record_per_page
         if (listSize < firstIndex or page <= 0):
             return (0, 0)
-        lastIndex = firstIndex + 5
+        lastIndex = firstIndex + maximum_record_per_page
         if (listSize < lastIndex):
             lastIndex = listSize
         return (firstIndex, lastIndex)
