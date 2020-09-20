b993c7af608e8b847e4899f84e9990e2ca5aa050
Tien Nguyen Thi Thuy <48781015+tienngthth@users.noreply.github.com>
2020-09-20 11:01:36 +0700

    Update bookingAPI.py

--- cloud/api/bookingAPI.py
+++ cloud/api/bookingAPI.py
@@ -58,7 +58,7 @@ def read():
         " Bookings ", 
         " WHERE CarID = %(car_id)s " +
         " AND CustomerID = %(customer_id)s " +
-        " AND RentTime <= NOW() AND NOW() <= ReturnTime AND Status = 'Booked'",
+        " AND RentTime <= NOW() AND NOW() <= ReturnTime AND Status = 'Booked' OR Status = 'In use'",
         {
             "car_id": request.args.get("car_id"),
             "customer_id": request.args.get("customer_id")
