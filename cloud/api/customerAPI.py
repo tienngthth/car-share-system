914c32083fc54673fa817d26de78e8dd613d32db
Cuong Nguyen Quoc <45681976+cuong24@users.noreply.github.com>
2020-09-21 18:18:09 +0700

    Update customerAPI.py

--- cloud/api/customerAPI.py
+++ cloud/api/customerAPI.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+customerAPI.py handles CRUD on customer users, and also a check if a given username exists.
+"""
 from flask import Blueprint, request
 from database import Database
 from flask.json import jsonify
@@ -8,6 +11,18 @@ customer_api = Blueprint("customer_api", __name__)
 
 @customer_api.route("/create", methods=['GET', 'POST'])
 def create():
+    """
+    Creates a customer. Parameters:
+    
+    username: The username requested
+    password: The user's requested password. Will be SHA256 hashed before storing.
+    first_name: The user's first name
+    last_name: The user's last name
+    email: The user's email address. Validated elsewhere by regex.
+    phone: The user's phone number
+    
+    Returns Success if an entry is created, and Fail otherwise.
+    """
     try:
         Database.insert_record_parameterized(
             "Customers(Username, Password, FirstName, LastName, Email, Phone)",
@@ -27,6 +42,18 @@ def create():
 
 @customer_api.route("/read")
 def read():
+    """
+    Searches for all customer users that match a set of criteria. Parameters:
+    id: The user's id
+    username: The user's username
+    first_name: The user's first name
+    last_name: The user's last name
+    email: The user's email address. Validated elsewhere by regex.
+    phone: The user's phone number
+    
+    Returns a dictionary containing all matches.
+    
+    """
     results = Database.select_record_parameterized(
         " * ", 
         " Customers ", 
@@ -55,6 +82,9 @@ def read():
 
 @customer_api.route("/update", methods=['GET', 'PUT'])
 def update():
+    """
+    Updates a user. Functions exactly like create()
+    """
     try:
         Database.update_record_parameterized(
             "Customers",
@@ -85,6 +115,13 @@ def update():
     
 @customer_api.route("/delete", methods=['GET', 'DELETE'])
 def delete():
+    """
+    Deletes a user. Parameters:
+    
+    id: The user id of the user to delete.
+    
+    Returns Success if an entry was deleted, and Fail otherwise.
+    """
     try:
         Database.delete_record_parameterized(
             " Customers ",
@@ -97,6 +134,13 @@ def delete():
 
 @customer_api.route("/get/id")
 def get_id():
+    """
+    Gets a user's id given their username. Parameters:
+    
+    username: The username of the user
+    
+    Returns invalid if no users found, or the user ID of the first result (there should only ever be one) otherwise.
+    """
     results = Database.select_record_parameterized(
         " ID ", 
         " Customers ", 
@@ -110,6 +154,13 @@ def get_id():
 
 @customer_api.route("/check/existed/username")
 def check_username():
+    """
+    Checks if a username exists. Parameters:
+    
+    username: the username of the user
+    
+    Returns 0 if the username does not exist, otherwise returns a count of the number of users with that name. Should never exceed 1.
+    """
     result = Database.select_record_parameterized(
         " COUNT(*) AS SUM ", 
         " Customers ", 
