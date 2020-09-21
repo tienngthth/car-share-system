d53195164c0e64d162eae98ed09742cdeb9996b6
Cuong Nguyen Quoc <45681976+cuong24@users.noreply.github.com>
2020-09-21 18:20:28 +0700

    Update locationAPI.py

--- cloud/api/locationAPI.py
+++ cloud/api/locationAPI.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-  
+"""
+locationAPI.py contains only a function to return details on a given location.
+"""
 from flask import Blueprint, request
 from database import Database
 from flask.json import jsonify
@@ -8,6 +11,13 @@ location_api = Blueprint("location_api", __name__)
 
 @location_api.route("get")
 def get_location():
+    """
+    This returns details (e.g. latitude, longitude) of a defined location. Parameters:
+    
+    id: The id of the location in question
+    
+    Returns a dictionary with the location details.
+    """
     results = Database.select_record_parameterized(
         " * ", 
         " Locations ",
